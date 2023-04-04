60177daabb4c6aaf0faf7c742229f0c8bbee789e
Olga <olga.sleepless@gmail.com>
2023-04-04 17:58:08 -0400

    Update ProfileForm

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginButton from './LoginButton';
+import logo from './logo.png';
 import './Navigation.css';
 
 function Navigation() {
@@ -25,7 +26,10 @@ function Navigation() {
   return (
     <header className="site-header">
       <NavLink exact to="/" className="nav-title">
-        <h3>airbbc</h3>
+        <div>
+          <img src={logo} alt="Logo" className="logo-img"/>
+          <h3>airbbc</h3>
+        </div>
       </NavLink>
       <div className="session-links">
         {sessionLinks}
