deae8038400776e5ada2abf35403d5ba5170b65a
Olga <olga.sleepless@gmail.com>
2023-07-13 22:30:14 -0400

    Update review

--- frontend/src/components/ReviewIndex/ReviewIndex.js
+++ frontend/src/components/ReviewIndex/ReviewIndex.js
@@ -1,19 +1,13 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import * as reviewActions from '../../store/reviews';
-import { Modal } from '../../context/Modal';
-import ReviewFormModal from '../ReviewForm/ReviewFormModal.js';
 import ReviewItem from '../ReviewItem/ReviewItem.js';
 import './ReviewIndex.css';
 
 
 const ReviewIndex = ({users, reviews, listing}) => {
-  const dispatch = useDispatch();
-
-  const user = useSelector(state => state.session.user); 
+  // const dispatch = useDispatch();
+  // const user = useSelector(state => state.session.user); 
 
   return (
-    <div className="review-show-container">
+    <div className="review_show_container">
       
       {reviews.map(review => (
         <ReviewItem 
